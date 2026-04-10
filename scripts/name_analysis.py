b300ffb65c8a00140ad4ed1aa93851a4de3c2345
Äyden <anpasma@st.noorderpoort.nl>
2026-04-10 09:57:21 +0200

    Toegevoegd: name_analysis script met API koppeling

---
+++ scripts/name_analysis.py
@@ -0,0 +1,20 @@
+import requests
+
+name = input("Voer een naam in: ")
+
+try:
+    age_response = requests.get("https://api.agify.io", params={"name": name})
+    gender_response = requests.get("https://api.genderize.io", params={"name": name})
+
+    age = age_response.json()["age"]
+    gender = gender_response.json()["gender"]
+
+    print("\nName:", name)
+    print("Predicted age:", age)
+    print("Predicted gender:", gender)
+
+except:
+    print("Er ging iets mis.")
+
+
+input("\nDruk op Een toets om af te sluiten...")
